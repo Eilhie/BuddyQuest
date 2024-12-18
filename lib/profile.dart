7dcf26a8d743ba3df8877148adfe45b5b7a065b8
Richie Darylson Tandiono <richie.tandiono@binus.edu>
2024-12-18 09:36:27 +0700

    load icon profile ketika buka page profile

--- lib/profile.dart
+++ lib/profile.dart
@@ -21,7 +21,6 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   void initState() {
     super.initState();
-    _loadProfileData();
   }
 
   Future<void> _loadProfileData() async {
@@ -67,184 +66,189 @@ class _ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween, // Place children on the left and right
-              children: [
-                IconButton(
-                  icon: Icon(Icons.arrow_back),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                ),
-                Text('Profile',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.settings),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => SettingsPage(),
-                      ),
-                    );
-                  },
-                ),
-              ],
-            ),
-            SizedBox(height: 40),
-            Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
+    return FutureBuilder(future: _loadProfileData(),
+        builder: (context, snapshot)
+        {
+          return Scaffold(
+            body: SingleChildScrollView(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween, // Place children on the left and right
                     children: [
-                      CircleAvatar(
-                        radius: 55,
-                        backgroundImage: AssetImage(_profileImage),
-                      ),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      ElevatedButton(
+                      IconButton(
+                        icon: Icon(Icons.arrow_back),
                         onPressed: () {
-                          print('Follow Button Pressed');
+                          Navigator.pop(context);
                         },
-                        child: Text("Follow"),
+                      ),
+                      Text('Profile',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.settings),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => SettingsPage(),
+                            ),
+                          );
+                        },
                       ),
                     ],
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 10),
-            Text(
-              _fullName,
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
+                  SizedBox(height: 40),
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            CircleAvatar(
+                              radius: 55,
+                              backgroundImage: AssetImage(_profileImage),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            ElevatedButton(
+                              onPressed: () {
+                                print('Follow Button Pressed');
+                              },
+                              child: Text("Follow"),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    _fullName,
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => FollowingFollowersPage()),
+                          );
+                        },
+                        child: RichText(
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: '$_followingCount ',
+                                style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey, fontSize: 16),
+                              ),
+                              TextSpan(
+                                text: 'Following     ',
+                                style: TextStyle(color: Colors.grey, fontSize: 16),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => FollowingFollowersPage()),
+                          );
+                        },
+                        child: RichText(
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: '$_followerCount ',
+                                style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey, fontSize: 16),
+                              ),
+                              TextSpan(
+                                text: 'Followers',
+                                style: TextStyle(color: Colors.grey, fontSize: 16),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Divider(
+                    color: Colors.grey,
+                    thickness: 1,
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Recent Posts',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  ..._userPosts.map((post) => _buildPostCard(post)).toList(),
+                ],
               ),
             ),
-            SizedBox(height: 5),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => FollowingFollowersPage()),
-                    );
-                  },
-                  child: RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                          text: '$_followingCount ',
-                          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey, fontSize: 16),
-                        ),
-                        TextSpan(
-                          text: 'Following     ',
-                          style: TextStyle(color: Colors.grey, fontSize: 16),
-                        ),
-                      ],
-                    ),
-                  ),
+            bottomNavigationBar: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              currentIndex: 3,
+              onTap: (index) {
+                if (index == 3) {
+                } else if (index == 1) {
+                  Navigator.pushNamed(context, '/workout');
+                } else if (index == 2) {
+                  Navigator.pushNamed(context, '/leaderboard');
+                } else if (index == 0) {
+                  Navigator.pushNamed(context, '/home');
+                }
+              },
+              selectedItemColor: Colors.deepPurple,
+              unselectedItemColor: Colors.black,
+              showSelectedLabels: true,
+              showUnselectedLabels: false,
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.pie_chart),
+                  label: 'Home',
                 ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => FollowingFollowersPage()),
-                    );
-                  },
-                  child: RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                          text: '$_followerCount ',
-                          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey, fontSize: 16),
-                        ),
-                        TextSpan(
-                          text: 'Followers',
-                          style: TextStyle(color: Colors.grey, fontSize: 16),
-                        ),
-                      ],
-                    ),
-                  ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.fitness_center),
+                  label: 'Workout',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.leaderboard),
+                  label: 'Leaderboard',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.person),
+                  label: 'Profile',
                 ),
               ],
             ),
-            SizedBox(height: 20),
-            Divider(
-              color: Colors.grey,
-              thickness: 1,
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Recent Posts',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            ..._userPosts.map((post) => _buildPostCard(post)).toList(),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: 3,
-        onTap: (index) {
-          if (index == 3) {
-          } else if (index == 1) {
-            Navigator.pushNamed(context, '/workout');
-          } else if (index == 2) {
-            Navigator.pushNamed(context, '/leaderboard');
-          } else if (index == 0) {
-            Navigator.pushNamed(context, '/home');
-          }
-        },
-        selectedItemColor: Colors.deepPurple,
-        unselectedItemColor: Colors.black,
-        showSelectedLabels: true,
-        showUnselectedLabels: false,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.pie_chart),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.fitness_center),
-            label: 'Workout',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.leaderboard),
-            label: 'Leaderboard',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
-      ),
-    );
+          );
+        });
+
   }
 
   Widget _buildPostCard(Map<String, dynamic> post) {
